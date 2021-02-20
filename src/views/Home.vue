b7b88929baa41c0eb52e6595bb5c934bb3446c9d
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-20 15:19:07 -0800

    project with vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-layout row wrap class="pa-6">
+    <v-flex v-for="movie in movies" xs4 class="pa-5" :key = "movie._id">
+      <v-card>
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">
+              <v-btn flat v-bind:to="`/movies/${movie._id}`">
+                {{movie.name}}
+              </v-btn>
+            </div>
+            <span class="grey-text">{{movie.release_year}}, {{movie.genre}}</span>
+          </div>          
+        </v-card-title>
+        <v-card-text>
+         {{movie.description}}
+        </v-card-text>
+      </v-card>
+    </v-flex> 
+  </v-layout>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// <script>
+// import axios from 'axios';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+// export default {
+//   name : 'Movies',
+//   data(){
+//     return {
+//       movies:[]
+//     };
+//   },
+//   mounted(){
+//    this.fetchMovies();
+//   },
+//   methods:{
+//     async fetchMovies(){
+//       console.log("Fetching Movies");
+//       const token = window.localStorage.getItem('auth');
+//       return axios({
+//         method:'get',
+//         url:'http://localhost:3000/movies',
+//         headers:{
+//           authorization: `JWT ${token}`,
+//          ' Content-Type': 'application/json'
+//         }
+//       })
+//       .then(res =>{
+//         this.movies = res.data.movies ;
+//         this.current_user = res.data.current_user;
+//       })
+//       .catch(err =>{
+//         return err ;
+//       })
+//     }
+//   }  
+// }
+// </script>
+
+
