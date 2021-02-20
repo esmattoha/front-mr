1f533b3ab71218241f5b0e8175f7bd59212f28b1
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-20 15:19:38 -0800

    project with vuetify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+// import Vueswal  from 'vue-swal';
 
 Vue.config.productionTip = false
 
+// Vue.use(Vueswal);
+
 new Vue({
+  el:'#app',
   router,
   store,
-  render: h => h(App)
+  vuetify,
+  components:{App},
+  template:'<App/>',
+  render: function (h) { return h(App) }
 }).$mount('#app')
